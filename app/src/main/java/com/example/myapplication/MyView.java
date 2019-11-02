2ce7a6c6ea5b2587075e0dbb0781bf9ec52113e4
Evgenya <subbotinjon01@mail.ru>
2019-11-02 18:14:22 +0300

    1.8.7 simple animation

--- app/src/main/java/com/example/myapplication/MyView.java
+++ app/src/main/java/com/example/myapplication/MyView.java
@@ -10,23 +10,99 @@ import android.view.View;
 public class MyView extends View {
     public MyView(Context context) {
         super(context);
+
+        for(int i = 0; i < N; i++){
+            lastTime[i] = System.currentTimeMillis();
+        }
     }
+
+    int N = 10;
+    float[] x  = new float[N];
+    float[] y  = new float[N];
+    float[] vx = new float[N];
+    float[] vy = new float[N];
+    int radius = 30;
+
+    float minV = 0.3f;
+
+    int[] colors = {
+            Color.GREEN,
+            Color.BLUE,
+            Color.rgb(105, 0, 5),
+            Color.rgb(64, 201, 87),
+            Color.rgb(89, 3, 105),
+            Color.RED,
+            Color.rgb(255, 102, 0),
+            Color.YELLOW,
+            Color.rgb(50, 168, 147),
+            Color.rgb(97, 50, 168)
+    };
+
+    long[] lastTime = new long[N];
+
+    Paint paint = new Paint();
+
+    boolean isStarted;
 
     @Override
     protected void onDraw(Canvas canvas) {
-        Paint paint = new Paint();
+        if(!isStarted){
+            isStarted = true;
 
-        paint.setColor(Color.GREEN);
-        paint.setStyle(Paint.Style.STROKE);
-        canvas.drawLine(getWidth(), 0, 0, getHeight(), paint);
+            for (int i = 0; i < N; i++) {
+                x[i] = (float) (Math.random() * getWidth());
+                y[i] = (float) (Math.random() * getHeight());
+                vx[i] = (float)(Math.max(Math.random() * 4 * minV, minV) - 2 * minV);
+                vy[i] = (float)(Math.max(Math.random() * 4 * minV, minV) - 2 * minV);
+            }
+        }
 
-        paint.setColor(Color.RED);
-        paint.setStyle(Paint.Style.FILL);
-        float
-                x = getWidth() / 2,
-                y = getHeight() / 2,
-                r = Math.min(getWidth(), getHeight()) / 3;
-        
-        canvas.drawCircle( x, y, r, paint);
+        for (int i = 0; i < N; i++) {
+            long nowTime = System.currentTimeMillis();
+
+            x[i] += vx[i] * (nowTime - lastTime[i]);
+            y[i] += vy[i] * (nowTime - lastTime[i]);
+
+            lastTime[i] = nowTime;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if(x[i] + radius >= getWidth()) vx[i] = -Math.abs(vx[i]);
+            if(x[i] - radius <= 0) vx[i] = Math.abs(vx[i]);
+
+            if(y[i] + radius >= getHeight()) vy[i] = -Math.abs(vy[i]);
+            if(y[i] - radius <= 0) vy[i] = Math.abs(vy[i]);
+        }
+
+        for(int i = 0; i < N; i++){
+            for(int j = i + 1; j < N; j++){
+                float dx = x[i] - x[j];
+                float dy = y[i] - y[j];
+
+                if(Math.sqrt(dx * dx + dy * dy) <= radius * 2){
+                    float kx = vx[i];
+                    float ky = vy[i];
+
+                    vx[i] = vx[j];
+                    vy[i] = vy[j];
+
+                    vx[j] = kx;
+                    vy[j] = ky;
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            paint.setStrokeWidth(3);
+            paint.setColor(Color.GRAY);
+            canvas.drawLine(x[i], y[i], x[(i + 1) % N], y[(i + 1) % N], paint);
+        }
+
+        for (int i = 0; i < N; i++) {
+            paint.setColor(colors[i]);
+            canvas.drawCircle(x[i], y[i], radius, paint);
+        }
+
+        invalidate();
     }
 }
