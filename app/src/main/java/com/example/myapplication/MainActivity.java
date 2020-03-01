a7bf55106b6ea8d88142de1ace76e184de70971f
Evgenya <subbotinjon01@mail.ru>
2020-03-01 20:51:31 +0300

    3.6.1 finish

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -12,31 +13,17 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Stack;
+
 public class MainActivity extends FragmentActivity {
-//    String[] operations = {
-//            "0",
-//            "1",
-//            "2",
-//            "3",
-//            "4",
-//            "5",
-//            "6",
-//            "7",
-//            "8",
-//            "9",
-//            "C",
-//            "+",
-//            "-",
-//            "*",
-//            "/",
-//            ".",
-//            "=",
-//            "BACKSPACE"
-//    };
 
     private static boolean hasPoint = false;
     private static boolean hasOperation = false;
     private static boolean hasResult = false;
+
+    private static int countChar = 0;
 
     @SuppressLint("StaticFieldLeak")
     private static TextView textView1, textView2;
@@ -45,6 +32,7 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         textView1 = findViewById(R.id.textView1);
         textView2 = findViewById(R.id.textView2);
@@ -66,7 +54,7 @@ public class MainActivity extends FragmentActivity {
                 hasPoint = false;
             }
 
-            if (hasOperation){
+            if (hasOperation && countChar != 0){
                 String newText = textView1.getText().toString();
                 newText = newText.substring(0, newText.length() - 1) + operation;
                 textView1.setText(newText);
@@ -83,13 +71,20 @@ public class MainActivity extends FragmentActivity {
                 hasPoint = false;
             }
 
-            if(textView1.getText().length() + textView2.getText().length() == 0
-                    && !operation.equals("-"))
+            if(countChar == 0 && (!operation.equals("-") || hasOperation) )
                 return;
 
             hasOperation = true;
 
-            String newText = "" + textView1.getText() + textView2.getText() + operation;
+            String newText;
+
+            if (textView1.getText().length() == 0){
+                newText = textView2.getText() + "\u200B" + operation;
+            }else {
+                newText = textView1.getText() + "\u200B" +
+                        textView2.getText() + "\u200B" + operation;
+            }
+
             textView1.setText(newText);
 
             textView2.setText("");
@@ -111,16 +106,19 @@ public class MainActivity extends FragmentActivity {
                 hasPoint = false;
             }
 
-            if(hasResult || textView1.getText().length() + textView2.getText().length() == 0)
+            if(hasResult || countChar == 0)
                 return;
 
             hasResult = true;
             hasPoint = false;
+            countChar = textView2.getText().length();
 
-            String newText = "" + textView1.getText() + textView2.getText() + operation;
+            String newText = textView1.getText() + "\u200B" +
+                    textView2.getText() + "\u200B" + operation;
+
             textView1.setText(newText);
 
-            String result = calculate() + "";
+            String result = calculate(textView1.getText().toString()) + "";
             textView2.setText(result);
             textView2.setTextColor(Color.GREEN);
         }else {
@@ -135,7 +133,7 @@ public class MainActivity extends FragmentActivity {
                 hasPoint = false;
             }
 
-            if(textView1.getText().length() + textView2.getText().length() + 1 > 30 ||
+            if(countChar > 30 ||
                     textView2.getText().length() + 1 > 9){
                 return;
             }
@@ -157,6 +155,7 @@ public class MainActivity extends FragmentActivity {
             }
 
             textView2.setText(newText);
+            countChar++;
         }
     }
 
@@ -178,21 +177,130 @@ public class MainActivity extends FragmentActivity {
         String newText = text.equals("") ?"" :text.substring(0, text.length() - 1);
 
         textView2.setText(newText);
+        if (text.length() != 0) countChar--;
     }
 
     public static void clear(){
         hasResult = false;
         hasPoint = false;
         hasOperation = false;
+        countChar = 0;
 
         textView1.setText("");
         textView2.setText("");
         textView2.setTextColor(Color.BLACK);
     }
 
-    private static double calculate(){
-        double result = 0;
+    private static double calculate(String string){
+        while (string.charAt(0) == '\u200B'){
+            string = string.substring(1);
+        }
 
-        return result;
+        if(string.charAt(0) == '-'){
+            string = "0\u200B" + string;
+        }
+
+        if(string.charAt(string.length() - 1) == '='){
+            string = string.substring(0, string.length() - 1);
+        }
+
+        String[] stings = string.split("\u200B");
+
+        ArrayList<String> arrayList = getPolishNotation(stings);
+
+        while (arrayList.indexOf("") >= 0){
+            arrayList.remove("");
+        }
+
+        return getResult(arrayList);
+    }
+
+    private static double getResult(ArrayList<String> arr){
+        ArrayList<Double> doubles = new ArrayList<>();
+
+        for (int i = 0; i < arr.size(); i++){
+            if(isDouble(arr.get(i))){
+                doubles.add(Double.parseDouble(arr.get(i)));
+            }else if(arr.get(i).equals("+")){
+                double tmp = doubles.get(doubles.size() - 2) + doubles.get(doubles.size() - 1);
+
+                doubles.remove(doubles.size() - 1);
+                doubles.remove(doubles.size() - 1);
+
+                doubles.add(tmp);
+            }else if(arr.get(i).equals("-")){
+                double tmp = doubles.get(doubles.size() - 2) - doubles.get(doubles.size() - 1);
+
+                doubles.remove(doubles.size() - 1);
+                doubles.remove(doubles.size() - 1);
+
+                doubles.add(tmp);
+            }else if(arr.get(i).equals("*")){
+                double tmp = doubles.get(doubles.size() - 2) * doubles.get(doubles.size() - 1);
+
+                doubles.remove(doubles.size() - 1);
+                doubles.remove(doubles.size() - 1);
+
+                doubles.add(tmp);
+            }else {
+                double tmp = doubles.get(doubles.size() - 2) / doubles.get(doubles.size() - 1);
+
+                doubles.remove(doubles.size() - 1);
+                doubles.remove(doubles.size() - 1);
+
+                doubles.add(tmp);
+            }
+        }
+
+        return doubles.get(0);
+    }
+
+    private static ArrayList<String> getPolishNotation(String[] arr){
+        ArrayList<String> newArr = new ArrayList<>();
+        Stack<String> stack = new Stack<>();
+
+        for(int i = 0; i < arr.length; i++){
+            if(isDouble(arr[i])){
+                newArr.add(arr[i]);
+            }else if(arr[i].equals(")")){
+
+                while (!stack.peek().equals("(")){
+                    newArr.add(stack.pop());
+                }
+
+                stack.pop();
+
+            }else if(arr[i].equals("+") || arr[i].equals("-")) {
+                while (!stack.empty() && (stack.peek().equals("+") || stack.peek().equals("-") ||
+                        stack.peek().equals("*") || stack.peek().equals("/"))){
+                    newArr.add(stack.pop());
+                }
+
+                stack.push(arr[i]);
+            }else if(arr[i].equals("*") || arr[i].equals("/")) {
+                while (!stack.empty() && (stack.peek().equals("*") || stack.peek().equals("/"))){
+                    newArr.add(stack.pop());
+                }
+
+                stack.push(arr[i]);
+            }else{
+                stack.push(arr[i]);
+            }
+        }
+
+        while (!stack.empty()){
+            newArr.add(stack.pop());
+        }
+
+        return newArr;
+    }
+
+    private static boolean isDouble(String s) throws NumberFormatException {
+        try {
+            Double.parseDouble(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
