38e8e482d92319d67bfa85b0de4716ade71c0ffe
Evgenya <subbotinjon01@mail.ru>
2020-03-14 16:58:55 +0300

    4.3 android-practice

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +11,62 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new TestSurfaceView(this));
+        setContentView(R.layout.activity_main);
+
+        MyCompanyAdapter adapter = new MyCompanyAdapter(this, makeCompanies());
+        ListView listView = findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+    }
+
+    MyCompany[] makeCompanies(){
+        MyCompany[] arr = new MyCompany[10];
+
+        String[] names = {
+                "Electronic Arts",
+                "Яндекс",
+                "Google",
+                "Apple",
+                "Facebook",
+                "IBM",
+                "Microsoft",
+                "Сбребанк",
+                "Oracle",
+                "Blizzard"
+        };
+
+        int[] pictures = {
+                R.drawable.electronic_arts,
+                R.drawable.yandex,
+                R.drawable.google,
+                R.drawable.apple,
+                R.drawable.facebook,
+                R.drawable.ibm,
+                R.drawable.microsoft,
+                R.drawable.sberbank,
+                R.drawable.oracle,
+                R.drawable.blizzard
+        };
+
+        double[] costs = {
+                97.06,
+                33.94,
+                1214.27,
+                277.97,
+                170.28,
+                107.95,
+                158.83,
+                2.71,
+                47.93,
+                59.04
+        };
+
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = new MyCompany();
+            arr[i].name = names[i];
+            arr[i].picture = pictures[i];
+            arr[i].cost = costs[i];
+        }
+
+        return arr;
     }
 }
