3cbd8f02635ad560a4374979762b0a66926693a1
Evgenya <subbotinjon01@mail.ru>
2020-04-14 14:58:40 +0300

    4.10.2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,59 +1,27 @@
 package com.example.myapplication;
 
-import android.database.Cursor;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.FragmentActivity;
-import androidx.loader.app.LoaderManager;
-import androidx.loader.content.CursorLoader;
-import androidx.loader.content.Loader;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends FragmentActivity implements LoaderManager.LoaderCallbacks<Cursor> {
-    private SimpleCursorAdapter adapter;
-
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView listView = findViewById(R.id.imageList);
+        EditText login_auth = findViewById(R.id.login_auth);
+        EditText pass_auth = findViewById(R.id.pass_auth);
 
-        adapter = new SimpleCursorAdapter(this, R.layout.my_item, null,
-                new String[]{
-                        MediaStore.Images.Thumbnails.DATA,
-                        MediaStore.Images.Thumbnails.DATA
-                },
-                new int[]{
-                        R.id.imageView,
-                        R.id.textView
-                }, 0);
+        ((Button)findViewById(R.id.auth)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        listView.setAdapter(adapter);
-    }
-
-    @NonNull
-    @Override
-    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
-        return new CursorLoader(this,
-                MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI,
-                new String[]{
-                        MediaStore.Images.Thumbnails._ID,
-                        MediaStore.Images.Thumbnails.DATA
-                }, null, null, null);
-    }
-
-    @Override
-    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
-        adapter.swapCursor(data);
-    }
-
-    @Override
-    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
-        adapter.swapCursor(null);
+            }
+        });
     }
 }
